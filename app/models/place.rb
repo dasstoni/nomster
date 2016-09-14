bd26f8409fcb43ee1e43765afbed0bdc487d2eaf
David Stonikas <dstonikas@gmail.com>
2016-09-14 18:55:50 +0000

    added last comment of place to index

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,4 +6,8 @@ class Place < ActiveRecord::Base
   validates :description, presence: true
   geocoded_by :address
   after_validation :geocode
+
+  def last_comment
+    self.comments.order("id ASC").last
+  end
 end
